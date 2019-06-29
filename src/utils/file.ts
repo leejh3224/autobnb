65f10cecfc0689bc707b67b9cfc760af3a20c66c
gompro <junhyung3224@gmail.com>
2019-06-30 02:40:56 +0900

    fix json parsing error

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,5 +1,6 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import reservations from '../../reservations.json';
 import { IReservation } from '../types';
 
 const File = () => {
@@ -13,8 +14,7 @@ const File = () => {
       );
     },
     async readReservationsList(): Promise<IReservation[]> {
-      const json = await fs.readFile(reservationsListPath);
-      return JSON.parse(json.toString());
+      return reservations;
     },
   };
 };
