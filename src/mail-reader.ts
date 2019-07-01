87fcb5574745b8d10e618bb18e86d7c19c1951f3
gompro <junhyung3224@gmail.com>
2019-07-01 21:07:08 +0900

    filter unseen

--- src/mail-reader.ts
+++ src/mail-reader.ts
@@ -22,7 +22,7 @@ const MailReader = async () => {
 
     await connection.openBox('AIRBNB');
 
-    const result = await connection.search(['ALL'], {
+    const result = await connection.search(['UNSEEN'], {
       bodies: ['TEXT'],
     });
     const transformPromises = await Promise.all(
